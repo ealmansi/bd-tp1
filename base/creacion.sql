93c44bbcd941a8a68c07739e0a36652f225ba621
Martin Fixman <martinfixman@gmail.com>
2016-05-01 19:25:11 -0300

    Todo terminado.

--- base/creacion.sql
+++ base/creacion.sql
@@ -98,19 +98,21 @@ CREATE TABLE compra (
 	idCompra INTEGER NOT NULL PRIMARY KEY,
 	fecha DATE NOT NULL,
 
+	idUsuario INTEGER NOT NULL,
 	idPago INTEGER NOT NULL,
 	idCalificacion INTEGER NOT NULL,
-	idPublicacion INTEGER NOT NULL,
+	idPublicacionFinalizada INTEGER NOT NULL,
+	FOREIGN KEY(idUsuario) REFERENCES usuario(idUsuario),
 	FOREIGN KEY(idPago) REFERENCES pago(idPago),
-	FOREIGN KEY(idCalificacion) REFERENCES calificacion(idCalificacion)
-	FOREIGN KEY(idPublicacion) REFERENCES publicacion(idPublicacion)
+	FOREIGN KEY(idCalificacion) REFERENCES calificacion(idCalificacion),
+	FOREIGN KEY(idPublicacionFinalizada) REFERENCES publicacionFinalizada(idPublicacion)
 );
 
 CREATE TABLE item (
 	idItem INTEGER NOT NULL PRIMARY KEY,
 
-	idPublicacion INTEGER NOT NULL
-	-- FOREIGN KEY(idPublicacion) REFERENCES publicacion(idPublicacion)
+	idPublicacion INTEGER NOT NULL,
+	FOREIGN KEY(idPublicacion) REFERENCES publicacion(idPublicacion)
 );
 
 CREATE TABLE producto (
@@ -139,4 +141,82 @@ CREATE TABLE tipoServicio (
 
 	nombreTipoPadre TEXT,
 	FOREIGN KEY(nombreTipoPadre) REFERENCES tipoServicio(nombreTipoPadre)
+);
+
+CREATE TABLE publicacion (
+	idPublicacion INTEGER NOT NULL PRIMARY KEY,
+	titulo TEXT NOT NULL,
+
+	idTipo INTEGER NOT NULL,
+	idUsuario INTEGER NOT NULL,
+	FOREIGN KEY(idTipo) REFERENCES tipoPublicacion(idTipo),
+	FOREIGN KEY(idUsuario) REFERENCES usuario(idUsuario)
+);
+
+CREATE TABLE favoritoUsuarioPublicacion (
+	idUsuario INTEGER NOT NULL,
+	idPublicacion INTEGER NOT NULL,
+	PRIMARY KEY(idUsuario, idPublicacion),
+	FOREIGN KEY(idUsuario) REFERENCES usuario(idUsuario),
+	FOREIGN KEY(idPublicacion) REFERENCES publicacion(idPublicacion)
+);
+
+CREATE TABLE visitaUsuarioPublicacion (
+	idUsuario INTEGER NOT NULL,
+	idPublicacion INTEGER NOT NULL,
+	PRIMARY KEY(idUsuario, idPublicacion),
+	FOREIGN KEY(idUsuario) REFERENCES usuario(idUsuario),
+	FOREIGN KEY(idPublicacion) REFERENCES publicacion(idPublicacion)
+);
+
+CREATE TABLE preguntaUsuarioPublicacion (
+	idPregunta INTEGER NOT NULL,
+	pregunta TEXT NOT NULL,
+	respuesta TEXT,
+
+	idUsuario INTEGER NOT NULL,
+	idPublicacion INTEGER NOT NULL,
+	PRIMARY KEY(idUsuario, idPublicacion, idPregunta),
+	FOREIGN KEY(idUsuario) REFERENCES usuario(idUsuario),
+	FOREIGN KEY(idPublicacion) REFERENCES publicacion(idPublicacion)
+);
+
+CREATE TABLE tipoPublicacion (
+	idTipo INTEGER NOT NULL PRIMARY KEY,
+	nombre TEXT NOT NULL,
+	comision FLOAT NOT NULL,
+	nivel INTEGER NOT NULL,
+	costo INTEGER NOT NULL,
+	caducidad DATE NOT NULL
+);
+
+CREATE TABLE publicacionVentaDirecta (
+	idPublicacion INTEGER NOT NULL PRIMARY KEY,
+	FOREIGN KEY(idPublicacion) REFERENCES publicacion(idPublicacion)
+);
+
+CREATE TABLE publicacionSubasta (
+	idPublicacion INTEGER NOT NULL PRIMARY KEY,
+	FOREIGN KEY(idPublicacion) REFERENCES publicacion(idPublicacion)
+);
+
+CREATE TABLE ofertaUsuarioSubasta (
+	precio INTEGER NOT NULL,
+	fechaOferta DATE NOT NULL,
+
+	idUsuario INTEGER NOT NULL,
+	idSubasta INTEGER NOT NULL,
+	PRIMARY KEY(idUsuario, idSubasta, fechaOferta),
+	FOREIGN KEY(idUsuario) REFERENCES usuario(idUsuario),
+	FOREIGN KEY(idSubasta) REFERENCES subasta(idPublicacion)
+);
+
+CREATE TABLE publicacionVigente (
+	idPublicacion INTEGER NOT NULL PRIMARY KEY,
+	FOREIGN KEY(idPublicacion) REFERENCES publicacion(idPublicacion)
+);
+
+CREATE TABLE publicacionFinalizada (
+	idPublicacion INTEGER NOT NULL PRIMARY KEY,
+	FOREIGN KEY(idPublicacion) REFERENCES publicacion(idPublicacion)
 );

3af2db346be419d856248c67624dc823005e470d
Emilio Almansi <ealmansi@gmail.com>
2016-05-06 15:58:11 -0300

    Sincronizo las queries con los cambios de esquema.

--- base/ofertar.py
+++ base/ofertar.py
@@ -3,8 +3,8 @@ import sys
 
 if __name__ == '__main__':
     bd = sys.argv[1]
-    params = {'idUsuario': sys.argv[2], 'idSubasta': sys.argv[3], 'precio': int(sys.argv[4])}
+    params = {'idUsuario': sys.argv[2], 'idPublicacion': sys.argv[3], 'monto': int(sys.argv[4])}
 
     con = sqlite3.connect(bd)
-    con.execute('INSERT INTO ofertaUsuarioSubasta VALUES (:idUsuario, :idSubasta, NOW(), :precio) VALUES', params)
+    con.execute('INSERT INTO hizoOferta VALUES (:idUsuario, :idPublicacion, NOW(), :monto) VALUES', params)
     con.commit()
--- base/triggers.sql
+++ base/triggers.sql
@@ -5,10 +5,10 @@
 PRAGMA foreign_keys = ON;
 
 -- Trigger para validar ingreso de oferta
-CREATE TRIGGER ofertaValida ON ofertaUsuarioSubasta BEFORE INSERT AS
- IF NEW.precio < (SELECT precio FROM ofertaUsuarioSubasta ORDER BY fechaOferta DESC LIMIT 1) 
-  OR NEW.precio > (SELECT precio FROM ofertaUsuarioSubasta ORDER BY fechaOferta DESC LIMIT 1) * 2
+CREATE TRIGGER ofertaValida ON hizoOferta BEFORE INSERT AS
+ IF NEW.monto < (SELECT monto FROM hizoOferta ORDER BY fecha DESC LIMIT 1) 
+  OR NEW.monto > (SELECT monto FROM hizoOferta ORDER BY fecha DESC LIMIT 1) * 2
  BEGIN
- RAISE (ABORT, 'El precio de la oferta debe superar el precio actual y ser inferior al doble del mismo');
+ RAISE (ABORT, 'El monto de la oferta debe superar el monto actual y ser inferior al doble del mismo');
  RETURN 
 END;

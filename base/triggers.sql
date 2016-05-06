04264d8c57800d348ba66b4200870ae4e8f04825
cgunski <cgunski@gmail.com>
2016-05-06 16:26:08 -0300

    Correccion de sintaxis en el trigger

--- base/triggers.sql
+++ base/triggers.sql
@@ -5,10 +5,9 @@
 PRAGMA foreign_keys = ON;
 
 -- Trigger para validar ingreso de oferta
-CREATE TRIGGER ofertaValida ON hizoOferta BEFORE INSERT AS
- IF NEW.monto < (SELECT monto FROM hizoOferta ORDER BY fecha DESC LIMIT 1) 
-  OR NEW.monto > (SELECT monto FROM hizoOferta ORDER BY fecha DESC LIMIT 1) * 2
- BEGIN
+CREATE TRIGGER ofertaValida BEFORE INSERT ON hizoOferta 
+ WHEN (NEW.monto < (SELECT monto FROM hizoOferta ORDER BY fecha DESC LIMIT 1) 
+  OR NEW.monto > (SELECT monto FROM hizoOferta ORDER BY fecha DESC LIMIT 1) * 2)
+BEGIN
  RAISE (ABORT, 'El monto de la oferta debe superar el monto actual y ser inferior al doble del mismo');
- RETURN 
 END;

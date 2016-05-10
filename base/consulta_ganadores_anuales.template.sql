4b7d92d438e52efcf5025b52936c4d251f229a7d
Emilio Almansi <ealmansi@gmail.com>
2016-05-10 17:20:49 -0300

    Edit consulta ganadores anuales. Devuelve la tabla con suma de ventas y valoracion promedio.

--- base/consulta_ganadores_anuales.template.sql
+++ base/consulta_ganadores_anuales.template.sql
@@ -1,7 +1,7 @@
 -- Consulta por ganador/es anual de viaje a Khan El-Khalili: obtener, para un año
 -- especı́fico, el ganador/es
 
-SELECT usuario.*, SUM(publicacion.precio) * AVG(calificacion.valoracionComprador) AS puntaje
+SELECT usuario.*, SUM(publicacion.precio) AS suma_precio_ventas,  AVG(calificacion.valoracionComprador) AS valoracion_promedio
 FROM usuario
 INNER JOIN publicacion ON usuario.idUsuario = publicacion.idUsuario
 INNER JOIN publicacionFinalizada ON publicacion.idPublicacion = publicacionFinalizada.idPublicacion
@@ -9,5 +9,3 @@ INNER JOIN compra ON publicacionFinalizada.idPublicacion = compra.idPublicacionF
 INNER JOIN calificacion ON compra.idCalificacion = calificacion.idCalificacion
 WHERE strftime('%Y', publicacion.fecha) = :año AND calificacion.valoracionComprador IS NOT NULL
 GROUP BY usuario.idUsuario
-ORDER BY puntaje DESC
-LIMIT 1;
